022e805f91f5430abfb983df94c051aacc78b21a
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-02 19:11:54 -0600

    Creating Movie App Skeleton Structure

---
+++ src/main/java/org/neoa/integration/movie/config/MovieConfiguration.java
@@ -0,0 +1,10 @@
+package org.neoa.integration.movie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("META-INF/spring/movie-app-integration.xml")
+public class MovieConfiguration {
+
+}
